04ce15cb83d4a805fb4ffc7e29e9446c3917f2a5
RaymondYi <odinjack@163.com>
2019-07-18 19:32:33 +0800

    增加多个中间件处理

--- app4.js
+++ app4.js
@@ -3,11 +3,21 @@ const Route = require("koa-router");
 const app = new Koa();
 const router = new Route();
 
+// 支持单个
 router.get(
-    '/users/:id', 
-    (ctx, next) => {
-        return User
-    },
-    (ctx, next) => {
+  "/users/:id",
+  (ctx, next) => {
+    return User.findOne(ctx.params.id).then(function(user) {
+      ctx.user = user;
+      next();
+    });
+  },
+  (ctx, next) => {
+    console.log(ctx.user);
+  }
+);
 
-    })
+app.use(router.routes());
+app.listen(3000, () => {
+  console.log('server listening port 3000');
+})
